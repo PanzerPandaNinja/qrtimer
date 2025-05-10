eaf9633469a73e0d3e64750425e76656ce84b142
PanzerPandaNinja <panzerpanda@gmail.com>
2025-05-10 09:43:54 +0200

    removed language buttons on main page and added pause translation

--- utils/interact.js
+++ utils/interact.js
@@ -1,4 +1,9 @@
-export function confirmPause (){
-    const userResponse = confirm("Restrart timer for this post? The elapsed time will be counted as a pause.")
+export function confirmPause (language = 'en') {
+    const lang = localStorage.getItem('qrtimer_language') || (navigator.language.startsWith('nb' || 'nn') ? 'no' : 'en');
+    const messages = lang === 'no' 
+        ? 'Starte tiden på nytt for denne posten? Den forløpte tiden vil bli regnet som en pause.' 
+        : 'Restart timer for this post? The elapsed time will be counted as a pause.';
+
+    const userResponse = confirm(messages);
     return userResponse;
 }
